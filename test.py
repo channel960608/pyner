e3c6bde5880ebcdc1d508e8ad108d283f25bacca
channel960608 <997476274@qq.com>
2019-11-05 17:40:37 +0800

    update

--- test.py
+++ test.py
@@ -2,15 +2,18 @@
 @Author: Caspar
 @Date: 2019-11-05 12:16:43
 @LastEditors: Caspar
-@LastEditTime: 2019-11-05 15:59:14
+@LastEditTime: 2019-11-05 17:40:29
 @Description: file content
 '''
 #%%
-import ner
+import nerer
 #%%
-tagger = ner.HttpNER(host='localhost', port=8080, location='/ner')
+tagger = nerer.HttpNER(host='localhost', port=8080, location='/ner')
 #%%
 tagger.get_entities("University of California is located in California, United States")
 #%%
 tagger.json_entities("Alice went to the Museum of Natural History.")
 
+
+
+# %%
